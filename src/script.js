beeaed69523d6e0d686f31476e0b7e95fac56e47
romeolacerda <romeolacerdafarias@gmail.com>
2024-01-24 16:42:39 -0300

    fim da pratica

--- src/script.js
+++ src/script.js
@@ -1,7 +1,29 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+let Calculadora = function(num1, num2, operador){
+  let resultado
+  number1 = Number(num1)
+  number2 = Number(num2)
+
+  if(operador = "+") resultado = number1 + number2
+  else if(operador = "-") resultado = number1 - number2
+  else if(operador = "*") resultado = number1 * number2
+  else if(operador = "/") resultado = number1 / number2
+  else if(operador = "e"){ resultado = Math.pow(num1, num2)
+    // let contador = 1
+    // resultado = num1 * num1
+    // while(contador < num2 - 1 ){
+    //   resultado = resultado * num1
+    //   contador ++ 
+    // }
+  }
+
+  if (resultado == undefined || resultado > 1000000) return("ERRO")
+
+  
+  return resultado
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
